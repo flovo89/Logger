b03c19b73d801857259f55488925145b68623b45
flovo <flovo@LAPTOP-FTQ6K5QH>
2017-04-04 06:39:51 +0200

    initial

---
+++ Logger/Logger.Host.LoggerStart/LoggerStart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+
+namespace Logger.Host.LoggerStart
+{
+    public static class LoggerStart
+    {
+        #region Static Methods
+
+        [STAThread]
+        public static int Main ()
+        {
+            return 1;
+        }
+
+        #endregion
+    }
+}
